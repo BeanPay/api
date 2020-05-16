c74efff51ce4baa5163f1101f4c6d8a4e26a0d1f
Brandon Romano <BrandonRRomano@gmail.com>
2020-05-16 15:57:17 -0700

    Fully implement User model with test suite (#6)
    * Adds tests for User Insert & Fetch
    
    * Adds full test suite to user model
    
    * Add one more test for coverage

--- database/models/user.go
+++ database/models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -37,28 +38,64 @@ type UserRepository struct {
 	DB *sql.DB
 }
 
-func (p *UserRepository) Insert(user User) (*User, error) {
+func (p *UserRepository) FetchByEmail(email string) (*User, error) {
 	row := p.DB.QueryRow(
-		"INSERT INTO users(email, password) VALUES($1, $2) RETURNING *;",
-		user.Email,
-		user.Password,
+		"SELECT * FROM users WHERE email = $1;",
+		email,
 	)
-	newUser := &User{}
-	err := newUser.consumeRow(row)
+	user := &User{}
+	err := user.consumeRow(row)
 	if err != nil {
 		return nil, err
 	}
-	return newUser, nil
+	return user, nil
 }
 
-func (p *UserRepository) Fetch(id string) {
-	return
+func (p *UserRepository) FetchByID(id string) (*User, error) {
+	row := p.DB.QueryRow(
+		"SELECT * FROM users WHERE id = $1;",
+		id,
+	)
+	user := &User{}
+	err := user.consumeRow(row)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
-func (p *UserRepository) Update(user User) error {
-	return nil
+func (p *UserRepository) Insert(user *User) error {
+	return user.consumeRow(
+		p.DB.QueryRow(
+			"INSERT INTO users(email, password) VALUES($1, $2) RETURNING *;",
+			user.Email,
+			user.Password,
+		),
+	)
 }
 
-func (p *UserRepository) Delete(user User) error {
+func (p *UserRepository) Update(user *User) error {
+	return user.consumeRow(
+		p.DB.QueryRow(
+			"UPDATE users SET email=$1, password=$2 WHERE id=$3 RETURNING *;",
+			user.Email,
+			user.Password,
+			user.Id,
+		),
+	)
+}
+
+func (p *UserRepository) Delete(user *User) error {
+	res, err := p.DB.Exec(
+		"DELETE FROM users WHERE id=$1;",
+		user.Id,
+	)
+	if err != nil {
+		return err
+	}
+	numRows, _ := res.RowsAffected()
+	if numRows != 1 {
+		return errors.New("Nothing was deleted.")
+	}
 	return nil
 }
---
+++ database/models/user_test.go
@@ -0,0 +1,77 @@
+package models
+
+import (
+	"github.com/beanpay/api/database"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUserRepo(t *testing.T) {
+	// Create a UserRepo
+	ephemeralDatabase, err := database.NewTestDatabase(
+		database.Config{
+			MigrationsDir: "../migrations",
+		},
+	)
+	assert.Nil(t, err)
+	defer ephemeralDatabase.Terminate()
+	userRepo := UserRepository{
+		DB: ephemeralDatabase.Connection(),
+	}
+
+	// Create a user
+	newUser := &User{
+		Email:    "some-email@example.com",
+		Password: "some-password",
+	}
+	err = userRepo.Insert(newUser)
+	assert.Nil(t, err)
+	assert.NotEqual(t, "", newUser.Id)
+
+	// Try to create the same user to ensure it's not created
+	err = userRepo.Insert(newUser)
+	assert.NotNil(t, err)
+
+	// Fetch the user by their ID
+	fetchedByIdUser, err := userRepo.FetchByID(newUser.Id)
+	assert.Nil(t, err)
+	assert.Equal(t, newUser.Email, fetchedByIdUser.Email)
+
+	// Fetch the user by their email address
+	fetchedByEmailUser, err := userRepo.FetchByEmail(newUser.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, newUser.Id, fetchedByEmailUser.Id)
+
+	// Fetch a user by a non-existing ID
+	failedFetchIdUser, err := userRepo.FetchByID("fake-id")
+	assert.NotNil(t, err)
+	assert.Nil(t, failedFetchIdUser)
+
+	// Fetch a user by a non-existing Email
+	failedFetchEmailUser, err := userRepo.FetchByEmail("fake-email")
+	assert.NotNil(t, err)
+	assert.Nil(t, failedFetchEmailUser)
+
+	// Update the users email
+	newUser.Email = "new-email@example.com"
+	err = userRepo.Update(newUser)
+	assert.Nil(t, err)
+
+	// Fetch the user by their new address
+	fetchedUpdatedUser, err := userRepo.FetchByEmail("new-email@example.com")
+	assert.Nil(t, err)
+	assert.Equal(t, newUser.Id, fetchedUpdatedUser.Id)
+
+	// Delete it
+	err = userRepo.Delete(newUser)
+	assert.Nil(t, err)
+
+	// Try to delete it again to ensure there's an error
+	err = userRepo.Delete(newUser)
+	assert.NotNil(t, err)
+
+	// Verify we can no longer fetch this user
+	fetchedDeletedUser, err := userRepo.FetchByID(newUser.Id)
+	assert.Nil(t, fetchedDeletedUser)
+	assert.NotNil(t, err)
+}
