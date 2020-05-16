fc167cc7cd5c7783bb2f3e5e92e102d5d7ce7607
BrandonRomano <BrandonRRomano@gmail.com>
2020-05-15 18:52:54 -0700

    Add more comprehensive converage to [POST] /users

--- server/users_test.go
+++ server/users_test.go
@@ -20,21 +20,81 @@ func (c *CreateUserBody) Read(p []byte) (n int, err error) {
 	return bytes.NewReader(b).Read(p)
 }
 
-func TestCreateUserSuccess(t *testing.T) {
+func TestCreateUser(t *testing.T) {
+	// Prepare the Server
 	server := NewTestServer()
 	defer server.Shutdown()
 
-	// Send Request
+	// Test that we are requiring the Email & Password fields
 	recorder := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodPost, "/users",
+		&CreateUserBody{},
+	)
+	server.createUser()(recorder, req)
+	assert.Equal(t,
+		response.Response{
+			StatusCode: http.StatusBadRequest,
+			StatusText: http.StatusText(http.StatusBadRequest),
+			ErrorDetails: &[]string{
+				"Email is a required field",
+				"Password is a required field",
+			},
+			Result: nil,
+		},
+		response.Parse(recorder.Result().Body),
+	)
+
+	// Test that we are only accepting valid email addresses
+	recorder = httptest.NewRecorder()
+	req = httptest.NewRequest(http.MethodPost, "/users",
+		&CreateUserBody{
+			Email:    "invalid-email",
+			Password: "some-password",
+		},
+	)
+	server.createUser()(recorder, req)
+	assert.Equal(t,
+		response.Response{
+			StatusCode: http.StatusBadRequest,
+			StatusText: http.StatusText(http.StatusBadRequest),
+			ErrorDetails: &[]string{
+				"Email must be a valid email address",
+			},
+			Result: nil,
+		},
+		response.Parse(recorder.Result().Body),
+	)
+
+	// Test that passwords must be at least 8 characters
+	recorder = httptest.NewRecorder()
+	req = httptest.NewRequest(http.MethodPost, "/users",
+		&CreateUserBody{
+			Email:    "name@example.com",
+			Password: "1234567",
+		},
+	)
+	server.createUser()(recorder, req)
+	assert.Equal(t,
+		response.Response{
+			StatusCode: http.StatusBadRequest,
+			StatusText: http.StatusText(http.StatusBadRequest),
+			ErrorDetails: &[]string{
+				"Password must be at least 8 characters in length",
+			},
+			Result: nil,
+		},
+		response.Parse(recorder.Result().Body),
+	)
+
+	// Test the Successful creation of a user
+	recorder = httptest.NewRecorder()
+	req = httptest.NewRequest(http.MethodPost, "/users",
 		&CreateUserBody{
 			Email:    "name@example.com",
 			Password: "some-password",
 		},
 	)
 	server.createUser()(recorder, req)
-
-	// Test
 	assert.Equal(t,
 		response.Response{
 			StatusCode: 200,
@@ -43,4 +103,26 @@ func TestCreateUserSuccess(t *testing.T) {
 		},
 		response.Parse(recorder.Result().Body),
 	)
+
+	// Test that we cannot create the same user twice
+	recorder = httptest.NewRecorder()
+	req = httptest.NewRequest(http.MethodPost, "/users",
+		&CreateUserBody{
+			Email:    "name@example.com",
+			Password: "some-password",
+		},
+	)
+	server.createUser()(recorder, req)
+	assert.Equal(t,
+		response.Response{
+			StatusCode: http.StatusConflict,
+			StatusText: http.StatusText(http.StatusConflict),
+			ErrorDetails: &[]string{
+				"Email is already in use by another user",
+			},
+			Result: nil,
+		},
+		response.Parse(recorder.Result().Body),
+	)
+
 }
