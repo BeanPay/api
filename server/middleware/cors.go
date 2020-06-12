b857462b87e0dc18b35cc28e78102ad1f6e2263d
BrandonRomano <BrandonRRomano@gmail.com>
2020-06-11 20:40:14 -0700

    Whitelist only the app URL for CORS

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -2,12 +2,14 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 )
 
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Origin", os.Getenv("APP_URL"))
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
 			w.Header().Add("Access-Control-Allow-Headers", "Authorization,Keep-Alive,User-Agent,Cache-Control,Content-Type")
